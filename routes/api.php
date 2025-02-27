6e1a75cdc4d60f37ce63af5b10a7a8f68b9470e8
MuhammadAtifKhan2 <muhammadatifkhan18@gmail.com>
2025-02-27 19:04:28 +0500

    API hit without login token, added functionality to restrict

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\SubCategoryController;
 use App\Http\Controllers\API\HomeController;
+use App\Http\Middleware\CheckUser;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 
 })->middleware('auth:api');
 
-Route::group(['middleware'=>'auth:api'],function(){
+Route::group(['middleware'=>CheckUser::class],function(){
     Route::get('test1',function(){ return response()->json('abc'); });
 });
 
