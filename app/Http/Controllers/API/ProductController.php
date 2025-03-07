5cdb340997790e306e9af800954a93e0162fd5b7
MuhammadAtifKhan2 <muhammadatifkhan18@gmail.com>
2025-03-07 00:40:48 -0800

    Product update,delete APIs

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -70,6 +70,19 @@ class ProductController extends Controller
     public function update(Request $request, string $id)
     {
         //
+         $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'category_id'=>'required',
+            'sub_category_id'=>'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json(['success'=>false,'errors'=>$validator->errors()]);
+        }
+
+        $product = Product::find($id)->update($request->all());
+        return response()->json(['success'=>true,'result'=>$product]);
     }
 
     /**
@@ -78,5 +91,8 @@ class ProductController extends Controller
     public function destroy(string $id)
     {
         //
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json(['success'=>true,'message'=>"Product Deleted Successfully"]);
     }
 }
